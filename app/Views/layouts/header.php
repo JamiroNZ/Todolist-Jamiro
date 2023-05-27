beb215a1dc7d2040499081d361efbf44701164e3
DinarSM <dinarsyibro1109@gmail.com>
2023-05-28 03:22:19 +0700

    1

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,7 @@
+<div class="contact">
+         <div class="container">
+            <div class="row ">
+               <div class="col-md-6">
+                  <div class="titlepage text_align_left">
+                     <h2>Get In Touch</h2>
+                  </div>
---
+++ app/Views/layouts/scripts.php
@@ -0,0 +1,4 @@
+<script src="/assets/js/jquery.min.js"></script>
+      <script src="/assets/js/bootstrap.bundle.min.js"></script>
+      <script src="/assets/js/jquery-3.0.0.min.js"></script>
+      <script src="/assets/js/custom.js"></script>
